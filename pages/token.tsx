c278a1aa6f450357a3a78d361c1617eee8798af0
ekinohito <zhenyagur9@gmail.com>
2021-07-08 20:46:09 +0300

    api

--- pages/token.tsx
+++ pages/token.tsx
@@ -1,9 +1,10 @@
 import TWContainer from "../components/tailwind/TWContainer";
 import React, {useState} from "react";
-import {setToken} from "../reducers/exam";
+import {setQuestions, setToken} from "../reducers/exam";
 import {useRouter} from "next/router";
 import NormalButton from "../components/Buttons/NormalButton";
 import {useDispatch} from "react-redux";
+import {Question} from "../types/exam";
 
 export default function Result() {
     const [text, setText] = useState('')
@@ -15,7 +16,32 @@ export default function Result() {
                 Пожалуйста, введите персональный токен:
                 <div className="flex flex-row w-full justify-between flex-wrap my-4">
                     <input className="border-b-2 border-blue-dark w-full sm:w-1/2" onChange={event => setText(event.target.value)}/>
-                    <NormalButton onClick={() => { dispatch(setToken(text)); router.push('/').then() }}>Ввести токен</NormalButton>
+                    <NormalButton onClick={async () => {
+                        const login = await fetch(process.env.NEXT_PUBLIC_BACKEND_SERVER + '/api/login', {
+                            method: "POST",
+                            body: JSON.stringify({
+                                token: text
+                            })
+                        })
+                        const exam = await fetch(process.env.NEXT_PUBLIC_BACKEND_SERVER + '/api/exam', {
+                            method: "POST",
+                            body: JSON.stringify({
+                                token: text
+                            })
+                        })
+                        if (!login.ok || !exam.ok) return
+                        dispatch(setToken(text))
+                        const data: {number: number, quest: string, answers: string[]}[] = (await exam.json()).task
+                        const questions: Question[] = data.map((value, index) => ({
+                            type: "variant",
+                            number: index + 1,
+                            question: value.quest,
+                            variants: value.answers,
+                            answer: undefined
+                        }))
+                        dispatch(setQuestions(questions))
+                        await router.push('/')
+                    }}>Ввести токен</NormalButton>
                 </div>
             </div>
         </TWContainer>
